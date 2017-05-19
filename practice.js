2bfe7ce827463a8a23e43faa3babb35c46479dfa
FinalFlashLight <ryan.godwin524@outlook.com>
2017-05-19 12:27:09 -0500

    finished practice

--- practice.js
+++ practice.js
@@ -1,14 +1,14 @@
 /*
    JAVASCRIPT PRACTICE
-   -- Each of the problems below ask you to write code in JavaScript. 
+   -- Each of the problems below ask you to write code in JavaScript.
    -- Behind the scenes, a file called 'test.spec.js' will check to see
-        if you return the correct values. 
+        if you return the correct values.
 
-   -- To run these tests and see the results, you simply need to open this 
+   -- To run these tests and see the results, you simply need to open this
         folder in a browser. The index page will show your results.
 
    -- NOTE:
-        If you have a syntax error anywhere in your code (e.g. missing brace), all tests will fail. Open your console in the browser to 
+        If you have a syntax error anywhere in your code (e.g. missing brace), all tests will fail. Open your console in the browser to
         find the line on which you have an error. Mentors can help with this.
 */
 
@@ -24,14 +24,14 @@
 
 
 // SAMPLE FAILING TEST
-// Notice what a failing test looks like in the test runner. 
+// Notice what a failing test looks like in the test runner.
 
 // Task
    // Define a function called speakFalsehood, which returns "Shea is the best" when called.
 
 // Incorrect Answer
    function speakFalsehood() {
-      return "Brian is amazing"
+      return "Shea is the best";
    }
 // Check the test
    // Click on the failing test (labeled "returns the truth") to see what you produced and what it expected
@@ -40,33 +40,78 @@
 // 1. isEven
    // Write a function called isEven, which takes in an integer as a parameter and returns true if the integer is even and false if the integer is odd.
 
-   // N.B. Return the boolean value true or false, not the string values of the words. 
+   // N.B. Return the boolean value true or false, not the string values of the words.
 
-   function isEven( ) {
-
+   function isEven(num) {
+     return num%2 === 0;
    }
 
 // 2. returnSmallest
   // Write a function called returnSmallest that takes in an array of numbers as a parameter and returns the smallest value in the array.
-
+function returnSmallest(arr){
+  smallest = arr[0];
+  for(i = 0; i < arr.length; ++i)
+  {
+    if (arr[i] < smallest){
+      smallest = arr[i];
+    }
+  }
+  return smallest;
+}
 
 // 3. addUser
   // Write a function called createUser, which takes in two parameters, an array of usernames and a new username. Create a new array which contains all of the usernames, including the new username. Return the new array.
-
+function addUser(arr, newUser)
+{
+  var newArr = [];
+  for (i = 0; i < arr.length; ++i)
+  {
+    newArr[i] = arr[i];
+  }
+  newArr.push(newUser);
+  return newArr;
+}
 // 4. reverseName
-   // Were you ever obsessed with saying your name backwards as a kid? I was for some reason. Write a function called reverseName which takes in a name and returns the reverse of the name. HINT: You can build a string through concatenation: 
+   // Were you ever obsessed with saying your name backwards as a kid? I was for some reason. Write a function called reverseName which takes in a name and returns the reverse of the name. HINT: You can build a string through concatenation:
    // var newString = oldString + 'a';
    // OR:
    // oldString += 'a';
+   function reverseName (name){
+     var arr=name.split("");
+     var newArr = [];
+     for(i = 0; i < arr.length; ++i)
+     {
+       newArr.unshift(arr[i]);
+     }
+     return newArr.join("");
+   }
 
-// 5. countVowels 
-   // Write a function called countVowels that takes in a string and returns the number of vowels in the string. 
-
+// 5. countVowels
+   // Write a function called countVowels that takes in a string and returns the number of vowels in the string.
+   function countVowels(str){
+     var vowels = "aeiouAEIOU";
+     var count = 0;
+     for(i = 0; i < str.length; ++i) {
+      if(vowels.indexOf(str[i]) >= 0)
+      {
+        ++count;
+      }
+     }
+     return count;
+   }
 // 6. alertMessage
    // Write a function called alertMessage, which alerts any message to the user.
-
+function alertMessage () {
+  alert("hi!");
+}
 // 7. promptMessage
-   // Write a function called promptMessage, which asks a user for a username using the window.prompt method. Return the username to the user. 
-
+   // Write a function called promptMessage, which asks a user for a username using the window.prompt method. Return the username to the user.
+function promptUsername () {
+  var username = window.prompt("enter your username!");
+  return username;
+}
 // 8. callbackPractice
-   // Write a function called callbackPractice that takes in two parameters, a string value and a callback function (you can call it cb). In your function, call the callback function (cb) and pass it the string value. 
+   // Write a function called callbackPractice that takes in two parameters, a string value and a callback function (you can call it cb). In your function, call the callback function (cb) and pass it the string value.
+function callbackPractice (str, cb) {
+  cb(str);
+}
